975e84571129153a556aaff5498da835708c401b
JustinArmour13 <hk6959@wayne.edu>
2025-10-23 08:31:57 -0400

    Committed wrong files his reverts commit dd0b6c1adfb267ffce1570ba4db678570a3ee905.

--- build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_MOST_FILE C:/Users/itsli/Documents/GitHub/MOST/build/Desktop_Qt_6_9_3_MinGW_64_bit-Debug/MOST.exe)
-set(__QT_DEPLOY_TARGET_MOST_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_MOST_RUNTIME_DLLS C:/Qt/6.9.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6Sql.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.3/mingw_64/bin/Qt6Core.dll)
